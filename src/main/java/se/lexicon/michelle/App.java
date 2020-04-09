6177c280d1adc953595272a63825370114c1f8cc
michelle <michelle.johansson85@gmail.com>
2020-04-09 08:40:17 +0200

    App.java: * Added Junit test example * created a while loop   - while !quit where quit is false as a default. * Switch added where only case q is currently working
    AppTest.java
    
    *two junit tests for the test example is added

--- src/main/java/se/lexicon/michelle/App.java
+++ src/main/java/se/lexicon/michelle/App.java
@@ -1,13 +1,74 @@
 package se.lexicon.michelle;
+import java.util.Scanner;
 
 /**
- * Hello world!
+ * Application need to be a Maven Project that runs with Java 8.
+ * Application need to be able to perform mathematical operations addition, subtraction, multiplication and division.
+ * The program should not stop running until the User choose to end it by terminating a loop.
+ * Each mathematical operation should be testable by Junit.
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    //Example for Junit testing
+    public static boolean isAdult(int age){
+        return age >= 18;
+    }
+
+    public static String isObjDouble( Scanner obj){
+        if(obj.hasNextDouble()){
+            double number = obj.nextDouble();
+            String state = "operator";
+            String choice = obj.nextLine();
+            return state;
+        }else {
+            System.out.println("The input was not a number! \n");
+            String choice = obj.nextLine();
+        }
+        return "number";
+    }
+    public static void main( String[] args ) {
+        double number;
+        boolean quit = false;
+        Scanner obj = new Scanner(System.in);
+        String choice;
+        String state = "number";
+
+        //The calculator will run util q is pressed.
+        while (!quit) {
+
+            if (state.equals("number")) {
+                System.out.println("Write a number:");
+                state = isObjDouble(obj);
+
+            } else {
+                System.out.println("What type of operator do you want to use? \n" +
+                        "( + , - , / , *, 'q' (to quit) )");
+                choice = obj.nextLine();
+                switch (choice) {
+                    case "q":
+                        quit = true;
+                        System.out.println("Thank you for your time!\n See you soon!");
+                        break;
+                    case "+":
+
+                        break;
+                    case "-":
+                        break;
+                    case "/":
+                        break;
+                    case "*":
+                        break;
+
+                    default:
+                        System.out.println("The value you typed is none of the suggested alternatives");
+
+                }
+                //choice = obj.nextLine();
+            }
+
+
+        }
+
     }
 }
--- src/test/java/se/lexicon/michelle/AppTest.java
+++ src/test/java/se/lexicon/michelle/AppTest.java
@@ -2,6 +2,7 @@ package se.lexicon.michelle;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -16,5 +17,19 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    /**
+     * Testing agefunction
+     */
+    @Test
+    public void test_isAdult_true(){
+        int age = 18;
+        Assert.assertTrue(App.isAdult((age)));
+    }
+    @Test
+    public void test_isAdult_false(){
+        int age = 17;
+        Assert.assertFalse(App.isAdult((age)));
     }
 }
