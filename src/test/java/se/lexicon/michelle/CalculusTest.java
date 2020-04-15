d99311bce3ff94700c0ad5d46746c1b465843bae
michelle <michelle.johansson85@gmail.com>
2020-04-15 14:04:24 +0200

    CalculusTest:  * testing division:   - given_positive_numbers_division_return_correct_sum: testing division with positive numbers.   - given_one_negative_and_one_positive_number_division_return_correct_sum: testing division with one negative and one positive number   - given_two_negative_number_division_return_correct_sum: testing division with negative numbers.   - given_number_divide_by_zero_return_false: testing when it is not division by zero   - given_number_divide_by_zero_return_true: testing when it is division by zero
    * Testing multiplication:
     - given_positive_numbers_multiplication_return_correct_sum: testing multiplication with only positive numbers
     - given_one_negative_and_one_positive_number_multiplication_return_correct_sum: testing multiplication with one negative and one positive number
     - given_two_negative_number_multiplication_return_correct_sum: testing multiplication with only negative numbers

--- src/test/java/se/lexicon/michelle/CalculusTest.java
+++ src/test/java/se/lexicon/michelle/CalculusTest.java
@@ -53,4 +53,101 @@ public class CalculusTest {
         Assert.assertEquals(expectedSum, actualSum, 0);
 
     }
+
+    /*
+       Testing division
+    */
+    @Test
+    public void given_positive_numbers_division_return_correct_sum(){
+        double firstNum = 10,
+                secondNum = 2,
+                actualSum =Calculus.divideNumbers(firstNum, secondNum),
+                expectedSum = 5 ;
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+    @Test
+    public void given_one_negative_and_one_positive_number_division_return_correct_sum(){
+        double firstNum = -10,
+                secondNum = 2,
+                actualSum = Calculus.divideNumbers(firstNum, secondNum),
+                expectedSum = -5 ;
+
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+    @Test
+    public void given_two_negative_number_division_return_correct_sum(){
+        double firstNum = -30,
+                secondNum = -15,
+                actualSum = Calculus.divideNumbers(firstNum, secondNum),
+                expectedSum = 2 ;
+
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+    /*
+      Test isDivisionByZero
+     */
+    @Test
+    public void given_number_divide_by_zero_return_false(){
+        double number = 30;
+
+        Assert.assertFalse(Calculus.isDivisionByZero(number));
+
+    }
+    @Test
+    public void given_number_divide_by_zero_return_true(){
+        double number = 0;
+        Assert.assertTrue(Calculus.isDivisionByZero(number));
+    }
+
+    /*
+      Testing multiplication
+   */
+    @Test
+    public void given_positive_numbers_multiplication_return_correct_sum(){
+        double firstNum = 10,
+                secondNum = 2,
+                actualSum =Calculus.multiplyNumbers(firstNum, secondNum),
+                expectedSum = 20 ;
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+    @Test
+    public void given_one_negative_and_one_positive_number_multiplication_return_correct_sum(){
+        double firstNum = -10,
+                secondNum = 2,
+                actualSum = Calculus.multiplyNumbers(firstNum, secondNum),
+                expectedSum = -20 ;
+
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+    @Test
+    public void given_two_negative_number_multiplication_return_correct_sum(){
+        double firstNum = -3,
+                secondNum = -5,
+                actualSum = Calculus.multiplyNumbers(firstNum, secondNum),
+                expectedSum = 15 ;
+
+
+        Assert.assertEquals(expectedSum, actualSum, 0);
+
+    }
+
+
+
+
+
 }
